daa9f6718d806773a119888e65721579244e231b
Natalied84 <n-dreher@hotmail.com>
2019-04-28 10:31:45 -0600

    pre-deploy v2

--- server.js
+++ server.js
@@ -33,9 +33,10 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/clickhole", {
-  useNewUrlParser: true
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true});
 
 // Routes
 
