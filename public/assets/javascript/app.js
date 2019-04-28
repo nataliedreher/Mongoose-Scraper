d6723c60c7fe6d325374079266dadd850101b230
Natalied84 <n-dreher@hotmail.com>
2019-04-28 11:41:29 -0600

    post deploy 1

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,44 +1,7 @@
-// Whenever someone clicks a p tag
-$(document).on("click", "p", function () {
-  // Empty the notes from the note section
-  $("#notes").empty();
-  // Save the id from the p tag
-  var thisId = $(this).attr("data-id");
-
-  // Now make an ajax call for the Article
-  $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-    // With that done, add the note information to the page
-    .then(function (data) {
-      console.log(data);
-      // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
-      // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
-      // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-      // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-      // If there's a note in the article
-      if (data.note) {
-        // Place the title of the note in the title input
-        $("#titleinput").val(data.note.title);
-        // Place the body of the note in the body textarea
-        $("#bodyinput").val(data.note.body);
-      }
-    });
-});
-
-// When you click the savenote button
+// When you click the save button
 $(".card").on("click", ".save", function () {
   // Grab the id associated with the article from the submit button
   var thisId = $(this).attr("data-id");
-  console.log($(this).attr("data-id"));
-  console.log($(this).parent().parent().parent().find(".card-body").text());
-  // console.log($(".article-link").text())
   // Run a POST request to change the note, using what's entered in the inputs
   $.ajax({
       method: "POST",
@@ -47,16 +10,28 @@ $(".card").on("click", ".save", function () {
         title: $(this).parent().find(".article-link").text(),
         link: $(this).parent().find(".article-link").attr("href"),
         summary: $(this).parent().parent().parent().find(".card-body").text()
-        // Value taken from note textarea
       }
     })
-    // With that done
     .then(function (data) {
       // Log the response
       console.log(data);
-
     });
+});
 
-  // Also, remove the values entered in the input and textarea for note entry
+$(".scrape-new").on("click", function() {
+  $.get("api/scrape", function(data) {
+    location.reload();
+  });
+});
 
+$(".clear").on("click", function() {
+  $.ajax({
+    method: "DELETE",
+    url: "/api/articles",
+    success: function(data) {
+    
+  }});
+ setTimeout(function(){
+    location.reload();
+  },60);
 });
--- routes/api/index.js
+++ routes/api/index.js
@@ -6,20 +6,16 @@ var db = require("../../models");
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// A GET route for scraping the echoJS website
 router.get("/scrape", function (req, res) {
     // First, we grab the body of the html with axios
     axios.get("https://www.clickhole.com/").then(function (response) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
-
         // Now, we grab every h2 within an article tag, and do the following:
         $("article").each(function (i, element) {
-
             // Save an empty result object
             var result = {};
             // Add the text and href of every link, and save them as properties of the result object
-
             result.title = $(this)
                 .find("h1")
                 .find("a")
@@ -31,48 +27,21 @@ router.get("/scrape", function (req, res) {
             result.summary = $(this)
                 .find("p")
                 .text()
-
             // Create a new Article using the `result` object built from scraping
             db.Article.create(result)
                 .then(function (dbArticle) {
                     // View the added result in the console
-                    // console.log(dbArticle);
+                    console.log(dbArticle);
                 })
                 .catch(function (err) {
                     // If an error occurred, log it
                     console.log(err);
                 });
-            // }
-        });
-
+         });
         // Send a message to the client
         res.send("Scrape Complete");
     });
 });
-
-// router.get("/saved/:id", function (req, res) {
-//     db.Saved.findOne({
-//             _id: req.params.id
-//         })
-//         .then(function (dbArticle) {
-//             // If we were able to successfully find Articles, send them back to the client
-//             db.Saved.create(dbArticle)
-//                 .then(function (dbSaved) {
-//                     res.json(dbSaved);
-
-//                 })
-//                 .catch(function (err) {
-//                     // If an error occurred, send it to the client
-//                     res.json(err);
-//                 });
-
-//         })
-//         .catch(function (err) {
-//             // If an error occurred, send it to the client
-//             res.json(err);
-//         });
-
-// })
 
 router.post("/saved", function (req, res) {
     // If we were able to successfully find Articles, send them back to the client
@@ -82,16 +51,12 @@ router.post("/saved", function (req, res) {
     db.Saved.create(data)
         .then(function (dbSaved) {
             // res.json(dbSaved);
-
         })
         .catch(function (err) {
             // If an error occurred, send it to the client
             res.json(err);
         });
-
-
 })
-
 
 router.get("/articles", function (req, res) {
     db.Article.find({})
@@ -106,7 +71,14 @@ router.get("/articles", function (req, res) {
 
 })
 
-
+router.delete("/articles", function(req, res) {
+    db.Article.deleteMany({}, function (err) {
+        if (err) return handleError(err);
+      });
+      db.Saved.deleteMany({}, function (err) {
+        if (err) return handleError(err);
+      });
+})
 
 
 module.exports = router;
